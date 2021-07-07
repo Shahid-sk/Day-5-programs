fc8312f2e86cc6e50b66cdb4d279a35d21915bc5
Shahid-sk <83329724+Shahid-sk@users.noreply.github.com>
2021-07-07 08:20:59 +0530

    Update vowel_consonant.java

--- src/vowel_consonant.java
+++ src/vowel_consonant.java
@@ -9,6 +9,8 @@ public class vowel_consonant {
 		char ch = s.next().charAt(0);
 		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
 			System.out.println(ch + " is a vowel");
+		else if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+			System.out.println(ch+ " is a vowel ");
 		else
 			System.out.println(ch + " is a consonant");
 	}
